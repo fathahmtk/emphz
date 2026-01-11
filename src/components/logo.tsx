33ce0e42ce4f39e754a824bd568cc4b7f74d948a
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2026-01-11 16:28:18 +0000

    Make betterlogo (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 const Logo = ({ className }: { className?: string }) => (
   <div className={cn("font-headline text-2xl font-black tracking-tighter text-primary", className)}>
-    EMPHZ
+    betterlogo
   </div>
 );
 
