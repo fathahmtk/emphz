1b1a8405bcec211bcb9e620e3858ec6eb70d34de
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2026-01-12 08:46:18 +0000

    add hero bg https://photos.fife.usercontent.google.com/pw/AP1GczORxYrgfs

--- src/components/home/entry-frame.tsx
+++ src/components/home/entry-frame.tsx
@@ -1,3 +1,4 @@
+
 import { Button } from "@/components/ui/button";
 import { getImage } from "@/lib/images";
 import Image from "next/image";
@@ -5,7 +6,7 @@ import Link from "next/link";
 import { ArrowRight, Phone } from 'lucide-react';
 
 export default function EntryFrame() {
-  const heroImage = getImage('hero-industrial');
+  const heroImage = getImage('hero-engineering');
   return (
     <section className="container">
       <div className="relative rounded-xxl bg-card border p-8 md:p-16 min-h-[400px] flex items-center overflow-hidden">
