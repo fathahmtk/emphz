7076b8aa03c385bf2b5d9cfa2df9fa4c84b1093a
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2026-01-12 06:14:21 +0000

    next

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,8 +91,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
+        'accordion-up': 'accordion-up 0.3s cubic-bezier(0.4, 0, 0.2, 1)',
       },
     },
   },
